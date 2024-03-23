9ff49dd367ced425fe787709dc4369e932d552ba
Ilia <118784244+Illialimonov@users.noreply.github.com>
2024-03-23 18:24:45 -0400

    no message

--- src/main/java/ua/lymonov/springMVC/config/MyWebAppInitializer.java
+++ src/main/java/ua/lymonov/springMVC/config/MyWebAppInitializer.java
@@ -1,6 +1,10 @@
 package ua.lymonov.springMVC.config;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
 
 public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -17,5 +21,16 @@ public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServl
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext servletContext) {
+        servletContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter())
+                .addMappingForUrlPatterns(null, true, "/*");
     }
 }
